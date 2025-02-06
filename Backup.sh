02ac298cf268136e033cef29e1605e00571051f7
Timeraider <57343973+GitTimeraider@users.noreply.github.com>
2025-02-06 17:24:41 +0100

    Create Backup.sh
    Basic backup script (will be further expanded/tested upon

---
+++ Backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo tar czf /backup.tar.gz \
+    --exclude=/backup.tar.gz \
+    --exclude=/dev \
+    --exclude=/mnt \
+    --exclude=/proc \
+    --exclude=/sys \
+    --exclude=/tmp \
+    --exclude=/media \
+    --exclude=/lost+found \
+    /
