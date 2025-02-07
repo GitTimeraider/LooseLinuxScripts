1a715cc4881a7c308b7eec51f83d1974f6cd810e
Timeraider <57343973+GitTimeraider@users.noreply.github.com>
2025-02-07 09:33:58 +0100

    Update Backup.sh

--- Backup/Backup.sh
+++ Backup/Backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Set the maximum number of backup files to keep
-MAX_BACKUPS=3
+BACKUP_LIMIT=3
 
 # Check for existing backup files and delete the oldest if there are already MAX_BACKUPS
 backup_count=$(ls /backup_*.tar.gz 2>/dev/null | wc -l)
-if [ "$backup_count" -ge "$MAX_BACKUPS" ]; then
+if [ "$backup_count" -ge "$BACKUP_LIMIT" ]; then
     oldest_backup=$(ls -t /backup_*.tar.gz | tail -1)
     rm "$oldest_backup"
 fi
