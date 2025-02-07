befcad784114cfa8b86c9100416a91f86a1af80c
Timeraider <57343973+GitTimeraider@users.noreply.github.com>
2025-02-07 10:57:16 +0100

    Update Backup.sh

--- Backup/Backup.sh
+++ Backup/Backup.sh
@@ -16,13 +16,18 @@ backup_file="/backup_$(date +%Y-%m-%d).tar.gz"
 # Get the size of the most recent backup file for progress calculation
 recent_backup=$(ls -t /backup_*.tar.gz | head -1)
 recent_backup_size=$(du -sb "$recent_backup" | awk '{print $1}')
+# Add 10% to the recent backup size
+target_size=$((recent_backup_size + recent_backup_size / 10))
 
 # Function to show progress bar
 show_progress() {
     local current_size
     while [ -d /proc/$1 ]; do
         current_size=$(du -sb "$backup_file" 2>/dev/null | awk '{print $1}')
-        progress=$((current_size * 100 / recent_backup_size))
+        progress=$((current_size * 100 / target_size))
+        if [ $progress -gt 100 ]; then
+            progress=100
+        fi
         echo -ne "\rProgress: $progress%"
         sleep 5
     done
