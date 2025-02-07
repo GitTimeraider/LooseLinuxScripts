9b3a0400d54518a420520307166e01ed9a36ddac
Timeraider <57343973+GitTimeraider@users.noreply.github.com>
2025-02-07 09:28:34 +0100

    Update Backup.sh

--- Backup/Backup.sh
+++ Backup/Backup.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Check for existing backup files and delete the oldest if there are already 3
+backup_count=$(ls /backup_*.tar.gz 2>/dev/null | wc -l)
+if [ "$backup_count" -ge 3 ]; then
+    oldest_backup=$(ls -t /backup_*.tar.gz | tail -1)
+    rm "$oldest_backup"
+fi
+
+# Create a new backup file with the current date
 sudo tar czf /backup_$(date +%Y-%m-%d).tar.gz \
     --exclude=/backup_$(date +%Y-%m-%d).tar.gz \
     --exclude=/dev \
